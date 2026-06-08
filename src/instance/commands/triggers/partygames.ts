3897420465b856936c7416db3a5f8ef4432d7e6a
beenycool <129210955+beenycool@users.noreply.github.com>
2026-06-08 12:46:16 +0000

    fix: correct party games API key names

--- src/instance/commands/triggers/partygames.ts
+++ src/instance/commands/triggers/partygames.ts
@@ -17,16 +17,16 @@ export default class PartyGames extends HypixelPlayerCommand {
     const arcade = player.stats?.arcade as Record<string, unknown> | undefined
     if (arcade === undefined) return `${givenUsername} has never played Arcade games.`
 
-    const wins1 = (arcade.party_games_wins_1 as number) ?? 0
-    const wins2 = (arcade.party_games_wins_2 as number) ?? 0
-    const wins3 = (arcade.party_games_wins_3 as number) ?? 0
-    const played = (arcade.party_games_played as number) ?? 0
-    const finisher = (arcade.party_games_finisher as number) ?? 0
+    const wins1 = (arcade.wins_party as number) ?? 0
+    const wins2 = (arcade.wins_party_2 as number) ?? 0
+    const wins3 = (arcade.wins_party_3 as number) ?? 0
+    const roundWins = (arcade.round_wins_party as number) ?? 0
+    const stars = (arcade.total_stars_party as number) ?? 0
 
     return (
       `${givenUsername}'s Party Games: ` +
-      `W1: ${shortenNumber(wins1)} | W2: ${shortenNumber(wins2)} | W3: ${shortenNumber(wins3)} | ` +
-      `Played: ${shortenNumber(played)} | Finisher: ${shortenNumber(finisher)}`
+      `1st: ${shortenNumber(wins1)} | 2nd: ${shortenNumber(wins2)} | 3rd: ${shortenNumber(wins3)} | ` +
+      `Round Wins: ${shortenNumber(roundWins)} | Stars: ${shortenNumber(stars)}`
     )
   }
 }
